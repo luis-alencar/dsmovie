0071c43ef492c3335ca2689bfaa53f0ff6f8aa3b
luis-alencar <luisfernando.fa@hotmail.com>
2022-01-24 09:34:24 -0300

    navbar

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
+import NavBar from './components/NavBar'
 
 
 ReactDOM.render(
